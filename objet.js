0ecf020cdc005ffd7ae86fbe5e1121316e6fc8ee
Thomas <thomas.besson28@gmail.com>
2020-02-04 13:27:33 +0100

    Add files via upload

--- objet.js
+++ objet.js
@@ -1,16 +1,12 @@
 var moto1;
 var moto2;
-var svgContainer = d3.select("body").append("svg").attr("width", 800).attr("height", 800); //on crée un espace ou mettre la moto
+var svgContainer = d3.select("body").append("svg").attr("width", 1000).attr("height", 600); //on crée un espace ou mettre la moto
 
-function InitGame() {
-    //moto1 = new Moto(400, 600, 1, "black"); //instance d'une moto
+function InitGame(){
     moto2 = new Moto(400, 550, 2, "red");
-    //moto1.dessinerMoto(0);
     moto2.dessinerMoto(0);
     
 }
-
-
 
 //définition de l'objet moto
 function Moto(X, Y, id_p, color){
@@ -18,14 +14,15 @@ function Moto(X, Y, id_p, color){
     this.X = X;
     this.Y = Y;
     this.speedX = 0;
-    this.speedY = 0;
+    this.speedY = -10;
     this.id_player = id_p;
     this.color = color;
     this.rectangle = null;
     this.rot = 0;
+    this.dir = null;
 
     this.dessinerMoto= function(){
-        this.rectangle = svgContainer.append("rect").attr("x", this.X).attr("y", this.Y).attr("width", 50).attr("height", 100).attr("fill", this.color).attr("id", "moto_html"+this.id_player).attr("transform", "rotate("+this.rot+","+(this.X-25)+","+(this.Y+75)+")");
+        this.rectangle = svgContainer.append("rect").attr("x", this.X).attr("y", this.Y).attr("width", 10).attr("height", 30).attr("fill", this.color).attr("id", "moto_html"+this.id_player).attr("transform", "rotate("+this.rot+","+(this.X+5)+","+(this.Y+25)+")");
     }
     this.movRight = function(){
         this.speedX += 10;
@@ -63,14 +60,12 @@ function Moto(X, Y, id_p, color){
     this.newPos = function() {
         this.X += this.speedX;
         this.Y += this.speedY;
-        this.speedX = 0;
-        this.speedY = 0;
     }
 }
 
 //on "raffraichit" la page , on va supprimer la moto et affciher une nouvelle moto avec les nouvelles coordonnées
 function Update(moto_update){
-    console.log("dans l'Update");
+    //console.log("dans l'Update");
     var elem = document.getElementById("moto_html"+moto_update.id_player);
     elem.parentNode.removeChild(elem);
     moto_update.newPos();
@@ -79,15 +74,115 @@ function Update(moto_update){
 
 //en paramètre on mettra l'id de la moto/joueur et on fera un switch pour les touches et on enclanchera la direction plus la rotation
 function Move(){
-    moto2.moveUPRIGHT();
     Update(moto2);
 }
 
-function rotation(moto_m){
+var touches = [];
 
-    moto_m.rot = 45;
-    d3.select("#moto_html"+moto_m.id_player).attr("transform","rotate(45,"+(moto_m.X-25)+","+(moto_m.Y+75)+")");
+d3.select("body")
+.on("keydown", function()
+{
+    var code = d3.event.keyCode;
+    if(touches.indexOf(code)<0) 
+        touches.push(code);
+    rotation(moto2);
+});
+
+d3.select("body")
+.on("keyup", function()
+{
+    var code = d3.event.keyCode;
+    index = touches.indexOf(code);
+    if(index>=0) 
+        touches.splice(index,1);
+}); 
+
+
+
+function direction()
+{
+    var dir=0;
+
+    if(touches.indexOf(37)!=-1) dir +=1; //left
+    if(touches.indexOf(38)!=-1) dir +=2; //up
+    if(touches.indexOf(39)!=-1) dir +=4; //right
+    if(touches.indexOf(40)!=-1) dir +=8; //down
+    
+    return dir;
 }
+
+
+function rotation(moto_m){
+    var dir =  direction();
+    moto_m.speedX = 0;
+    moto_m.speedY = 0;
+    var rotX = moto_m.X;
+    var rotY = moto_m.Y;
+    console.log(rotX+", "+rotY);
+    
+          switch (dir) {               
+              case 1:
+                  ori = 'O';
+                  moto_m.rot = 270;
+                  moto_m.moveleft();
+                  Update(moto_m);
+
+              break;
+              case 2:
+                  ori = 'N';
+                  moto_m.rot = 0;
+                  moto_m.moveup();
+                  Update(moto_m);
+
+              break;
+              case 3:
+                  ori = 'NO';
+                  moto_m.rot = 315;
+                  moto_m.moveUPLEFT();
+                  Update(moto_m);
+
+              break;
+              case 4:
+                  ori = 'E';
+                  moto_m.rot = 90;
+                  moto_m.movRight();
+                  Update(moto_m);
+
+              break;
+              case 6:
+                  ori = 'NE';
+                  moto_m.rot = 45;
+                  moto_m.moveUPRIGHT();
+                  Update(moto_m);
+
+              break;
+              case 8:
+                  ori = 'S';
+                  moto_m.rot = 180;
+                  moto_m.movedown();
+                  Update(moto_m);
+
+              break;
+              case 9:
+                  ori = 'SE';
+                  moto_m.rot = 225;
+                  moto_m.moveDOWNLEFT();
+                  Update(moto_m);
+
+              break;
+              case 12:
+                  ori = 'NE';
+                  moto_m.rot = 135;
+                  moto_m.moveDOWNRIGTH();
+                  Update(moto_m);
+
+              break;
+          default : console.log("Aucune touche valide")
+           // il faut check si l'orientation ets correcte
+          }
+          
+}
+
 
 InitGame();
 
