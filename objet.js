c49af44344f053ba1d8b9d19eb58612ea2b6c685
Thomas <thomas.besson28@gmail.com>
2020-02-04 18:25:27 +0100

    Add files via upload

--- objet.js
+++ objet.js
@@ -3,8 +3,10 @@ var moto2;
 var svgContainer = d3.select("body").append("svg").attr("width", 1000).attr("height", 600); //on crée un espace ou mettre la moto
 
 function InitGame(){
+    moto1 = new Moto(350,550 , 1, "black");
     moto2 = new Moto(400, 550, 2, "red");
-    moto2.dessinerMoto(0);
+    moto1.dessinerMoto();
+    moto2.dessinerMoto();
     
 }
 
@@ -19,62 +21,71 @@ function Moto(X, Y, id_p, color){
     this.color = color;
     this.rectangle = null;
     this.rot = 0;
-    this.dir = null;
+    this.ori = "N";
 
     this.dessinerMoto= function(){
+        //on dessine la moto avec déjà les attributs rotation que l'on modifiera avec l'attribut this.rot
         this.rectangle = svgContainer.append("rect").attr("x", this.X).attr("y", this.Y).attr("width", 10).attr("height", 30).attr("fill", this.color).attr("id", "moto_html"+this.id_player).attr("transform", "rotate("+this.rot+","+(this.X+5)+","+(this.Y+25)+")");
     }
     this.movRight = function(){
-        this.speedX += 10;
+        this.speedX = 10;
     }
     this.moveleft = function(){
-        this.speedX -= 10;
+        this.speedX = -10;
     }
     this.moveup = function(){
-        this.speedY -= 10;
+        this.speedY = -10;
     }
     this.movedown = function(){
-        this.speedY += 10;
+        this.speedY = 10;
     }
 
     this.moveUPRIGHT = function(){
-        this.speedX += 10;
-        this.speedY -= 10;
+        this.speedX = 7.1;
+        this.speedY = -7.1;
     }
 
     this.moveUPLEFT= function(){
-        this.speedX -= 10;
-        this.speedY -= 10;
+        this.speedX = -7.1;
+        this.speedY = -7.1;
     }
 
     this.moveDOWNLEFT= function(){
-        this.speedX -= 10;
-        this.speedY += 10;
+        this.speedX = -7.1;
+        this.speedY = 7.1;
     }
 
     this.moveDOWNRIGTH = function(){
-        this.speedX += 10;
-        this.speedY += 10;
+        this.speedX = 7.1;
+        this.speedY = 7.1;
     }
 
     this.newPos = function() {
         this.X += this.speedX;
-        this.Y += this.speedY;
+        this.Y += this.speedY;  
     }
 }
 
 //on "raffraichit" la page , on va supprimer la moto et affciher une nouvelle moto avec les nouvelles coordonnées
 function Update(moto_update){
     //console.log("dans l'Update");
+
+    //on selectionne la moto dans le corps html
     var elem = document.getElementById("moto_html"+moto_update.id_player);
+
+    //on retire la moto de la plateforme on l'efface (Attention on ne la détruit pas !!!)
     elem.parentNode.removeChild(elem);
+    //on lui donne la nouvelle direction à prendre
     moto_update.newPos();
+    //on redessine la moto avec les nouvelle coordonné et la nouvelle direction
     moto_update.dessinerMoto();
 }
 
-//en paramètre on mettra l'id de la moto/joueur et on fera un switch pour les touches et on enclanchera la direction plus la rotation
 function Move(){
+    //on rafracihi la moto
+    rotation(moto2);
     Update(moto2);
+    Update(moto1);
 }
 
 var touches = [];
@@ -85,7 +96,6 @@ d3.select("body")
     var code = d3.event.keyCode;
     if(touches.indexOf(code)<0) 
         touches.push(code);
-    rotation(moto2);
 });
 
 d3.select("body")
@@ -95,14 +105,12 @@ d3.select("body")
     index = touches.indexOf(code);
     if(index>=0) 
         touches.splice(index,1);
-}); 
-
-
+});
 
 function direction()
 {
     var dir=0;
-
+    //code pour chaque touche
     if(touches.indexOf(37)!=-1) dir +=1; //left
     if(touches.indexOf(38)!=-1) dir +=2; //up
     if(touches.indexOf(39)!=-1) dir +=4; //right
@@ -111,81 +119,142 @@ function direction()
     return dir;
 }
 
-
 function rotation(moto_m){
     var dir =  direction();
-    moto_m.speedX = 0;
+    moto_m.speedX = 0; //la direction est mise à 0 en X et Y
     moto_m.speedY = 0;
-    var rotX = moto_m.X;
-    var rotY = moto_m.Y;
-    console.log(rotX+", "+rotY);
-    
           switch (dir) {               
               case 1:
-                  ori = 'O';
-                  moto_m.rot = 270;
-                  moto_m.moveleft();
-                  Update(moto_m);
+                  if(moto_m.ori=="NO" || moto_m.ori=="SO"){
+                    moto_m.ori = 'O';
+                    moto_m.rot = 270; //on donne la rotation à la moto pour quand elle sera redessiné
+                    moto_m.moveleft(); //on bouge la moto dans la direction
+                  }else{
+                    avancedefault(moto_m);
+                }
 
               break;
               case 2:
-                  ori = 'N';
-                  moto_m.rot = 0;
-                  moto_m.moveup();
-                  Update(moto_m);
+                if(moto_m.ori=="NO" || moto_m.ori=="NE"){
+                    moto_m.ori = 'N';
+                    moto_m.rot = 0;
+                    moto_m.moveup();
+                }else{
+                    avancedefault(moto_m);
+                }
+
 
               break;
               case 3:
-                  ori = 'NO';
-                  moto_m.rot = 315;
-                  moto_m.moveUPLEFT();
-                  Update(moto_m);
+                if(moto_m.ori=="O" || moto_m.ori=="N"){
+                    moto_m.ori = 'NO';
+                    moto_m.rot = 315;
+                    moto_m.moveUPLEFT();
+                }else{
+                    avancedefault(moto_m);
+                }
+
 
               break;
               case 4:
-                  ori = 'E';
-                  moto_m.rot = 90;
-                  moto_m.movRight();
-                  Update(moto_m);
+                if(moto_m.ori=="NE" || moto_m.ori=="SE"){
+                    moto_m.ori = 'E';
+                    moto_m.rot = 90;
+                    moto_m.movRight();
+                }else{
+                    avancedefault(moto_m);
+                }
+
 
               break;
               case 6:
-                  ori = 'NE';
-                  moto_m.rot = 45;
-                  moto_m.moveUPRIGHT();
-                  Update(moto_m);
+                if(moto_m.ori=="N" || moto_m.ori=="E"){
+                    moto_m.ori = 'NE';
+                    moto_m.rot = 45;
+                    moto_m.moveUPRIGHT();
+                }else{
+                    avancedefault(moto_m);
+                }
 
               break;
               case 8:
-                  ori = 'S';
-                  moto_m.rot = 180;
-                  moto_m.movedown();
-                  Update(moto_m);
+                if(moto_m.ori=="SE" || moto_m.ori=="SO"){
+                    moto_m.ori = 'S';
+                    moto_m.rot = 180;
+                    moto_m.movedown();
+                }else{
+                    avancedefault(moto_m);
+                }
+
 
               break;
               case 9:
-                  ori = 'SE';
-                  moto_m.rot = 225;
-                  moto_m.moveDOWNLEFT();
-                  Update(moto_m);
+                if(moto_m.ori=="O" || moto_m.ori=="S"){
+                    moto_m.ori = 'SO';
+                    moto_m.rot = 225;
+                    moto_m.moveDOWNLEFT();
+                }else{
+                    avancedefault(moto_m);
+                }
+
 
               break;
               case 12:
-                  ori = 'NE';
-                  moto_m.rot = 135;
-                  moto_m.moveDOWNRIGTH();
-                  Update(moto_m);
+                if(moto_m.ori=="E" || moto_m.ori=="S"){
+                    moto_m.ori = 'SE';
+                    moto_m.rot = 135;
+                    moto_m.moveDOWNRIGTH();
+                }else{
+                    avancedefault(moto_m);
+                }
+
 
               break;
-          default : console.log("Aucune touche valide")
+          default : avancedefault(moto_m);
            // il faut check si l'orientation ets correcte
           }
           
 }
 
+function avancedefault(moto_m){
+    console.log(moto_m.ori, moto_m.rot);
+    switch (moto_m.ori){
+        case "N":
+            moto_m.moveup();
+        break;
+
+        case "S":
+            moto_m.movedown();
+        break;
+        
+        case "O":
+            moto_m.moveleft();
+            break;
+        
+        case "E":
+            moto_m.movRight();
+            break;
+            
+        case "NO":
+            moto_m.moveUPLEFT();
+            break;
+        
+        case "NE":
+            moto_m.moveUPRIGHT();
+            break;
+
+        case "SO":
+            moto_m.moveDOWNLEFT();
+            break;
+        
+        case "SE":
+            moto_m.moveDOWNRIGTH();
+            break;
+        default: console.log("pas d'oritentation");
+    }
+
+}
 
 InitGame();
 
-var myVar = setInterval(Move, 60);
-
-
+var myVar = setInterval(Move, 10, moto2);
